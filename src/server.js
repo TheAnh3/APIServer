3c5dab253d6ddd9913d89cb629b5cbdf7dd6af8c
Filip <fakenews.comp@gmail.com>
2024-09-11 21:30:49 +0200

    Setup

--- src/server.js
+++ src/server.js
@@ -1,3 +1,13 @@
 // SERVER
 
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+app.get('/', (req,res) => {
+    res.send('Hello Kahoun!');
+});
+
+app.listen(PORT, () => {
+    console.log('Running on http://localhost:${PORT}')
+});
